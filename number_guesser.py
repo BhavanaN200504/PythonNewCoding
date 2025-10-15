cfbc5ad220cf8e8b38bb35d410c3d65dcd6547d0
BhavanaN200504 <bhavana0435@gmail.com>
2025-10-15 19:48:16 +0530

    Add files via upload

--- number_guesser.py
+++ number_guesser.py
@@ -15,9 +15,10 @@ else:
     
 
 random_number = random.randint(0, top_of_range)
-
+guesses = 0
 
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -29,5 +30,11 @@ while True:
         print("You got it!")
         break
     else:
-        print("You got it Wrong!")
+        if user_guess == random_number:
+            print("You were above the number! ")
+        else:
+            print("You were below the number! ")
+        
+        
+print("You got it in", guesses, "guesses")
         
